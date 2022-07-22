a7f0af7d7cc65c438688c304a30d094160aa901f
akdfkeks <akdfkeks@gmail.com>
2022-07-22 14:41:12 +0900

    .

--- app/electron/worker/detector.js
+++ app/electron/worker/detector.js
@@ -1,21 +1,13 @@
 const faceapi = require("face-api.js");
 const path = require("path");
 
-// Global variables and flags
 let cam;
-let loopTimer;
-let isReady = false;
-let isPaused = true;
-const displaySize = {
-	width: 480,
-	height: 360,
-};
-
-// Initialize detect option and model
+const displaySize = { width: 480, height: 360 }; //
+const overlay = document.getElementById("overlay");
+const dims = faceapi.matchDimensions(overlay, displaySize, true);
 const minConfidence = 0.1;
 const faceapiOptions = new faceapi.SsdMobilenetv1Options({ minConfidence });
 
-// Basic environment settings
 faceapi.env.monkeyPatch({
 	Canvas: HTMLCanvasElement,
 	Image: HTMLImageElement,
@@ -30,12 +22,11 @@ async function loadNet() {
 	await faceapi.loadFaceDetectionModel(path.join(__dirname, "../data/weights"));
 }
 
-// Initialize Camera
 async function initCamera(width, height) {
-	const video = document.getElementById("cam");
 	video.width = width;
 	video.height = height;
 
+	const video = document.getElementById("cam");
 	const stream = await navigator.mediaDevices.getUserMedia({
 		audio: false,
 		video: {
@@ -53,9 +44,6 @@ async function initCamera(width, height) {
 	});
 }
 
-const overlay = document.getElementById("overlay");
-const dims = faceapi.matchDimensions(overlay, displaySize, true);
-
 async function detectFace() {
 	const frame = await faceapi.detectSingleFace(cam, faceapiOptions);
 	if (frame) {
@@ -69,20 +57,11 @@ async function detectFace() {
 
 loadNet()
 	.then((_) => {
-		console.log("Network has loaded");
-		return initCamera(displaySize.width, displaySize.height);
+		console.log("Model has loaded");
+		return await initCamera(displaySize.width, displaySize.height);
 	})
 	.then((video) => {
 		console.log("Camera was initialized");
-		cam = video;
-		isReady = true;
-		camController(true);
+		cam = video
+		setInterval(detectFace, 0);
 	});
-
-function camController(flag) {
-	if (isReady && isPaused && flag) {
-		loopTimer = setInterval(detectFace, 0);
-	} else {
-		clearInterval(loopTimer);
-	}
-}
