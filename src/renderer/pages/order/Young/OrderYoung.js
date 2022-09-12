34e5fbbb194559de8e4a7ca94f9d6bfc6588797e
leehh6024 <dlgusghchlrh@gmail.com>
2022-09-12 21:18:53 +0900

    캐로셀구현

--- src/renderer/pages/order/Young/OrderYoung.js
+++ src/renderer/pages/order/Young/OrderYoung.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from "react";
-import "./Order.css";
-import { Link } from "react-router-dom";
+import React from "react";
+import BestMenu from "./BestMenu";
 import Menucategory from "./Menucategory";
 
-const orderYoung = () => {
-  const menus = useRef([
-    {
-      // 여기에 추천메뉴들 들어가야함
-    },
-  ]);
-
-  const [current, setCurrent] = useState(0);
-  const [style, setStyle] = useState({
-    marginLeft: `-${current}00%`,
-  });
-  const menuSize = useRef(menus.current.length);
-
-  const moveSlide = (i) => {
-    let nextIndex = current + i;
-
-    if (nextIndex < 0) nextIndex = menuSize.current - 1;
-    else if (nextIndex > menuSize.current) nextIndex = 0;
-
-    setCurrent(nextIndex);
-  };
-
-  useEffect(() => {
-    setStyle({ marginLeft: `-${current}00%` });
-  }, [current]);
-
+function orderYoung() {
   return (
     <>
-      <div className="bestMenu-title">추천메뉴</div>
-      <div className="bestMenu-box">
-        <div className="bestMenu-slide">
-          <button className="best-menu1"></button>
-          <button className="best-menu2"></button>
-          <button className="best-menu3"></button>
-          <button className="best-menu4"></button>
-        </div>
-      </div>
-      <hr className="line"></hr>
+      {/* <Header />
+    추천메뉴 */}
+      <BestMenu />
       <Menucategory />
+
+      {/* <Footer />
+    장바구니에 담길 메뉴 몇가지 + 결제하기 버튼 + */}
     </>
   );
-};
+}
 
 export default orderYoung;
